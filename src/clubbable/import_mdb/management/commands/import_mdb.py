5da2152dc71d1371a8e1a0545bda81533be15d20
Norman Hooper <norman@kaapstorm.com>
2015-09-11 14:36:33 +0200

    Added import_mdb management command

---
+++ src/clubbable/import_mdb/management/commands/import_mdb.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.core.management import BaseCommand
+from import_mdb.import_mdb import import_mdb
+
+
+class Command(BaseCommand):
+    help = 'Imports from a Microsoft Access database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('-f', '--file', help='MDB file')
+
+    def handle(self, *args, **options):
+        import_mdb(options['file'])
