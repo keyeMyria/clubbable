a4faeb689cd32a97dff78f37ed8394e2acc9808d
Norman Hooper <norman@kaapstorm.com>
2015-09-11 19:24:38 +0200

    get user's full name

--- src/clubbable/website/views.py
+++ src/clubbable/website/views.py
@@ -1,11 +1,20 @@
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from markdown import markdown
-import yaml
+
+
+def get_full_name(user):
+    member = user.profile.member
+    if member:
+        return '%s' % member
+    full_name = user.get_full_name()
+    if full_name:
+        return full_name
+    if user.email:
+        return user.email
+    return user.username
 
 
 @login_required
 def dashboard(request):
-    context = {}
+    context = {'full_name': get_full_name(request.user)}
     return render(request, 'website/dashboard.html', context)
