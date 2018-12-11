20a71eca462322096e8ff3d987a1691581e64ace
Norman Hooper <norman@kaapstorm.com>
2018-12-11 15:31:34 +0200

    Delete empty tests

--- src/clubbable/website/tests.py
+++ src/clubbable/website/tests.py
@@ -1,7 +1,7 @@
 from contextlib import contextmanager
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.urls import reverse
 
 
 @contextmanager
